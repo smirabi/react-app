e6b9adc857e9f9f808641512923ff1a53910a1a9
Sasha Mirabi <sasha.mirabi@gmail.com>
2017-10-09 20:04:05 -0700

    made a beautiful search component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/SearchBar'
 
+
+const App = () => {
+    
+        return (
+            <div>
+                {<SearchBar />}
+                {/* <h1>Hello there World!</h1>
+                <p>I love saying hello world</p> */}
+            </div>
+        )
+    }
+
+ReactDOM.render(<App/>, document.getElementById('root'))
 registerServiceWorker();
